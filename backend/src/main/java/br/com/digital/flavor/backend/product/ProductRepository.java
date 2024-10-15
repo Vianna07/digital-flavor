a8e6801a76af01313a279cfa2575e2f4871d369e
Vianna07 <viannagustavo71@gmail.com>
2024-10-15 20:50:12 -0300

    fix: corrige tipo de Id no ProductRepository

--- backend/src/main/java/br/com/digital/flavor/backend/product/ProductRepository.java
+++ backend/src/main/java/br/com/digital/flavor/backend/product/ProductRepository.java
@@ -3,6 +3,8 @@ package br.com.digital.flavor.backend.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 }
