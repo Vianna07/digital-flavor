9ef57d820dc4e485c7a5087da0b0bc984b097fe9
Vianna07 <viannagustavo71@gmail.com>
2024-10-15 20:59:20 -0300

    refactor: altera tipo @Controller para @RestController no UserController
    adiciona @RequestMapping("/user")

--- backend/src/main/java/br/com/digital/flavor/backend/user/UserController.java
+++ backend/src/main/java/br/com/digital/flavor/backend/user/UserController.java
@@ -1,7 +1,9 @@
 package br.com.digital.flavor.backend.user;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/user")
 public class UserController {
 }
