dc1153850a308e70fe10d33a396b38fe2e353590
Vianna07 <viannagustavo71@gmail.com>
2024-10-26 17:50:17 -0300

    refactor: altera build da imagem do backend
    retira inotify do build pois pode ser ineficiente ficar monitorando dezenas de
    arquivos
    para que tenha alterações será apenas exutado o intelliJ com as
    variavéis de ambiente
    para debugar o container o agentlib continua no build para conseguir
    entrar remotamente na aplicação java

--- scripts/entrypoint-spring-app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mvn spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:5005" &
-
-while true; do
-    inotifywait -r -e modify,create,delete,move src/
-    mvn compile
-    sleep 2
-done
