1ffa30e270a14116e5ea8959706d665bc3991027
Vianna07 <viannagustavo71@gmail.com>
2024-10-26 23:55:01 -0300

    style: formata código

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -14,23 +14,32 @@ export default {
 				'text-secondary': '#555555',
 				border: 'rgba(0, 0, 0, 0.1)',
 				hover: '#E7E7E7',
-				'card-bg': '#ffffff',
-		  	},
-		  	fontFamily: {
+				'card-bg': '#ffffff'
+			},
+			fontFamily: {
 				body: [
-					'Sora', 'Arial', '-apple-system', 'BlinkMacSystemFont', 
-					'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 
-					'Open Sans', 'Helvetica Neue', 'sans-serif'
-				],
-		  	},
-		  	spacing: {
+					'Sora',
+					'Arial',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'Oxygen',
+					'Ubuntu',
+					'Cantarell',
+					'Open Sans',
+					'Helvetica Neue',
+					'sans-serif'
+				]
+			},
+			spacing: {
 				'column-width': '42rem',
-				'column-margin-top': '4rem',
-		  	},
-		  	transitionDuration: {
-				'speed': '300ms',
-		  	},
-		},
+				'column-margin-top': '4rem'
+			},
+			transitionDuration: {
+				speed: '300ms'
+			}
+		}
 	},
 
 	plugins: []
