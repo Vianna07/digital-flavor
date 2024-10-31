d76c38cc79f903d7c08fc9f0d81ae25dc670a839
Vianna07 <viannagustavo71@gmail.com>
2024-10-30 23:10:27 -0300

    feat: adiciona layout para página dos formulários iniciais

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<main class="flex h-full w-full flex-col items-center justify-center">
+	{@render children()}
+</main>
+
+<style></style>
