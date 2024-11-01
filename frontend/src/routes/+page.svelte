25951072f86513b8375526261df33898d336a0b2
Vianna07 <viannagustavo71@gmail.com>
2024-11-01 10:37:24 -0300

    feat: adiciona página para representar formulário

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import NameInput from '@components/form/inputs/Name.svelte';
+	import EmailInput from '@components/form/inputs/Email.svelte';
+	import PasswordInput from '@components/form/inputs/Password.svelte';
+	import eyeIcon from '@icons/eye.svg';
+	import type { PasswordInputProps } from '$lib/types';
+
+	const password: PasswordInputProps = {
+		id: 'retype-password',
+		hasEye: false,
+	};
+</script>
+
+<form class="flex flex-col gap-5" action="">
+	<NameInput />
+	<EmailInput />
+	<PasswordInput />
+	<PasswordInput {...password} />
+</form>
+
+<style>
+</style>
