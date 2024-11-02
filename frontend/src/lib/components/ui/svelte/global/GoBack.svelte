ee32dace43d60b70832a97251fdc08cec00f4590
Vianna07 <viannagustavo71@gmail.com>
2024-11-02 20:44:20 -0300

    feat: cria componente GoBack, para voltar a página anterior

---
+++ frontend/src/lib/components/ui/svelte/global/GoBack.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { GoBackProps } from '$lib/types';
+	import keyboardArrowLeft from '@icons/keyboard-arrow-left.svg';
+
+  let { iconUrl, redirectUrl }: GoBackProps = $props();
+</script>
+
+<a href={redirectUrl}>
+  <button type="button" class="primary">
+    <img src={iconUrl ?? keyboardArrowLeft} alt="Arrow-left" />
+  </button>
+</a>
