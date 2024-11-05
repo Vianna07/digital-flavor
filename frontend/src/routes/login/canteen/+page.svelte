8e8505675930205737911689951f6d301a72fc4d
Vianna07 <viannagustavo71@gmail.com>
2024-11-05 20:30:12 -0300

    feat: cria página de listagem de cantinas

---
+++ frontend/src/routes/login/canteen/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import logo from '@images/logo.png';
+	import GenericList from '@components/global/GenericList.svelte';
+	import GenericInput from '@components/global/GenericInput.svelte';
+	import searchIcon from '@icons/magnifying-glass.svg';
+	import type { Canteen, GenericInputProps, GenericListProps } from '$lib/types';
+	import { goto } from '$app/navigation';
+
+	let { data }: { data: { canteens: Canteen[] } } = $props();
+
+	const list: GenericListProps<Canteen> = {
+		list: data.canteens,
+		fields: {
+			title: 'name',
+			subTitle: 'address'
+		},
+		onclick: (id: string) => {
+			goto('/home', {
+				state: id
+			});
+		}
+	};
+
+	const searchInput: GenericInputProps = {
+		id: 'search',
+		type: 'text',
+		label: 'Pesquise pela cantina',
+		leftIconUrl: searchIcon
+	};
+</script>
+
+<section class="page">
+	<header>
+		<img src={logo} alt="logo" />
+	</header>
+
+	<div class="content">
+		<GenericInput {...searchInput} />
+		<GenericList {...list} />
+	</div>
+
+	<footer>
+		<p>Selecione a loja que deseja comprar conforme a sua localização</p>
+	</footer>
+</section>
+
+<style lang="postcss">
+	.page {
+		@apply flex flex-col items-center justify-center gap-10;
+
+		width: clamp(18rem, 20rem, 24rem);
+
+		header {
+			@apply absolute top-10 flex items-center justify-center;
+
+			img {
+				@apply h-48 w-48;
+			}
+		}
+
+		.content {
+			@apply flex w-full max-w-full flex-col gap-24;
+		}
+
+		footer {
+			@apply absolute bottom-10 max-w-full px-10 text-center;
+		}
+	}
+</style>
