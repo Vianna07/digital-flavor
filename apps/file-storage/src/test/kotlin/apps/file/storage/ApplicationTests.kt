be825251e55c4f0f6cf716623b0fe1714e424a54
Vianna07 <viannagustavo71@gmail.com>
2024-11-25 21:09:26 -0300

    feat: cria API em kotlin com spring boot para lidar com o serviço da Amazon S3

---
+++ apps/file-storage/src/test/kotlin/apps/file/storage/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package apps.file.storage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
