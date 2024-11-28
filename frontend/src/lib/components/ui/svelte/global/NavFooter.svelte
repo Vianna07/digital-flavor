1164c4bf17a71a4eb5ffe6b1b4836006f20971b4
Vianna07 <viannagustavo71@gmail.com>
2024-11-28 19:19:30 -0300

    refactor: adpta NavFooter com tailwindcss e potscss ao invés de css puro

--- frontend/src/lib/components/ui/svelte/global/NavFooter.svelte
+++ frontend/src/lib/components/ui/svelte/global/NavFooter.svelte
@@ -13,174 +13,101 @@
   ];
 
   let activeIndex = 0;
-  let isAnimating = false;
-
 
   function navigateWithAnimation(index: number, href: string) {
-    if (isAnimating) return; // Se já está animando, ignora o clique
-
-    isAnimating = true;
     activeIndex = index;
-
-
-    setTimeout(() => {
-      goto(href);
-    }, 500);
-  }
-
-
-  function onTransitionEnd() {
-    isAnimating = false;
   }
 </script>
 
-<div class="navigation">
-  <ul>
-    {#each icons as icon, i}
-      <li class:active={activeIndex === i}
-          on:click={() => navigateWithAnimation(i, icon.href)}
-          class="icon-item"
-          on:transitionend={onTransitionEnd}>
-        <a href="#">
-          <span class="icon">
-            <img src={icon.icon} class="svg-icon" alt="icon" />
-          </span>
-        </a>
-      </li>
-    {/each}
-    <div class="indicator" style="transform: translateX(calc(80px * {activeIndex} - 115px));">
-      <span></span>
-    </div>
-  </ul>
-</div>
+<footer>
+  <nav>
+    <ul>
+      {#each icons as {icon, href}, i}
+        <li class:active={activeIndex === i}
+            class="icon-item">
+          <a href={href} on:click={() => navigateWithAnimation(i, href)}>
+            <span class="icon">
+              <img src={icon} alt="icon" />
+            </span>
+          </a>
+        </li>
+      {/each}
+      <div class="indicator" style="transform: translateX(calc(80px * {activeIndex} - 115px));">
+        <span></span>
+      </div>
+    </ul>
+  </nav>
+</footer>
 
-<style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+<style lang="postcss">
+  footer, nav {
+    @apply w-full h-[4.375rem] bg-primary flex justify-center items-center rounded-xl;
   }
 
-  :root {
-    --clr-background: #F9F2ED;
-    --clr-primary: #ff0000;
-    --clr-secondary: #EDD6C8;
-    --clr-dark: #313131;
-    --clr-light: #E3E3E3;
-  }
+  nav {
+    ul {
+      @apply relative flex justify-center items-center w-full;
 
-  body {
-    display: flex;
-    justify-content: center;
-    align-items: end;
-    min-height: 100vh;
-    background: var(--clr-background);
-  }
+      li {
+        @apply relative list-none w-20 h-[4.375rem] z-10 cursor-pointer;
 
-  .navigation {
-    width: 105%;
-    height: 70px;
-    background: var(--clr-primary);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
-    border-radius: 10px;
-  }
+        a {
+          @apply relative flex justify-center items-center w-full;
 
-  .navigation ul {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-  }
+          .icon {
+            @apply relative block text-[2em] text-light opacity-85;
 
-  .navigation ul li {
-    position: relative;
-    list-style: none;
-    width: 80px;
-    height: 70px;
-    z-index: 1;
-    cursor: pointer;
-  }
+            transition: opacity 0.3s, transform 0.3s;
 
-  .navigation ul li a {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-  }
+            img {
+              @apply w-[1.875rem] h-[3.75rem] ml-[0.3125rem];
 
-  .navigation ul li a .icon {
-    position: relative;
-    display: block;
-    font-size: 2em;
-    color: var(--clr-secondary);
-    opacity: 0.85;
-    transition: opacity 0.3s, transform 0.3s;
-  }
+              transition: transform 0.3s ease;
+            }
+          }
+        }
 
-  .navigation ul li.active a .icon {
-    opacity: 1;
-    transform: translateY(-7px);
-    filter: brightness(0) saturate(100%) invert(12%) sepia(82%) saturate(7499%) hue-rotate(1deg) brightness(96%) contrast(116%);
-    transition: opacity 0.3s, transform 0.3s;
-  }
+        &.active {
+          a {
+            .icon {
+              @apply opacity-100 -translate-y-[0.4375rem];
 
-  .svg-icon {
-    width: 30px;
-    height: 60px;
-    margin-left: 5px;
-    transition: transform 0.3s ease;
-  }
+              filter: brightness(0) invert(12%) sepia(82%) saturate(50);
+              transition: opacity 0.3s, transform 0.3s;
+            }
+          }
+        }
+      }
 
-  .indicator {
-    position: absolute;
-    top: -10px;
-    width: 70px;
-    height: 70px;
-    background: var(--clr-background);
-    border-bottom-left-radius: 35px;
-    border-bottom-right-radius: 35px;
-    border: 6px solid var(--clr-background);
-    cursor: pointer;
-    transition: transform 0.5s ease;
-  }
+      .indicator {
+        @apply absolute -top-[0.625rem] w-[4.375rem] h-[4.375rem] bg-light rounded-bl-[2.1875rem] rounded-br-[2.1875rem] border-[6px] border-solid border-light cursor-pointer;
 
-  .indicator::before {
-    content: "";
-    position: absolute;
-    top: 4px;
-    left: -25.75px;
-    width: 20px;
-    height: 20px;
-    background: transparent;
-    border-top-right-radius: 20px;
-    box-shadow: 4px -6px 0 2px var(--clr-background);
-  }
+        transition: transform 0.5s ease;
 
-  .indicator::after {
-    content: "";
-    position: absolute;
-    top: 4px;
-    right: -25.75px;
-    width: 20px;
-    height: 20px;
-    background: transparent;
-    border-top-left-radius: 20px;
-    box-shadow: -4px -6px 0 2px var(--clr-background);
-  }
+        &::before, &::after {
+          @apply absolute top-1 w-5 h-5 bg-transparent;
 
-  .indicator span {
-    position: absolute;
-    left: -3px;
-    width: 60px;
-    height: 60px;
-    border: 4px solid var(--clr-primary);
-    border-radius: 50%;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
-    transform: scale(0.85);
+          content: "";
+        }
+
+        &::before {
+          @apply -left-[1.609375rem]  rounded-tr-[1.25rem];
+
+          box-shadow: 4px -6px 0 2px theme('colors.light');
+        }
+
+        &::after {
+          @apply -right-[1.609375rem]  rounded-tl-[1.25rem];
+
+          box-shadow: -4px -6px 0 2px theme('colors.light');
+        }
+
+        span {
+          @apply absolute w-[3.75rem] h-[3.75rem] rounded-[50%] scale-[0.85] border-4 border-solid border-primary -left-[0.1875rem];
+
+          box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+        }
+      }
+    }
   }
 </style>
