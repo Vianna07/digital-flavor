7168c8356f6a837a5c10ec2c3c53390bc30fca08
Vianna07 <viannagustavo71@gmail.com>
2024-11-28 17:22:43 -0300

    feat: cria classes que recebem os valores dos targets aws.s3 e aws.cloud-front

--- apps/file-storage/src/main/kotlin/apps/file/storage/Application.kt
+++ apps/file-storage/src/main/kotlin/apps/file/storage/Application.kt
@@ -1,11 +1,15 @@
 package apps.file.storage
 
+import apps.file.storage.cloud_front.CloudFrontProperties
+import apps.file.storage.s3.S3Properties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(S3Properties::class, CloudFrontProperties::class)
 class Application
 
 fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+    runApplication<Application>(*args)
 }
