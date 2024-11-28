1f2cfde1a65ee02838bb65399511ab1f530b6eb2
Vianna07 <viannagustavo71@gmail.com>
2024-11-28 20:34:15 -0300

    feat: adiciona margin no footer

--- frontend/src/lib/components/ui/svelte/global/NavFooter.svelte
+++ frontend/src/lib/components/ui/svelte/global/NavFooter.svelte
@@ -44,68 +44,72 @@
     @apply w-full h-[4.375rem] bg-primary flex justify-center items-center rounded-xl;
   }
 
-  nav {
-    ul {
-      @apply relative flex justify-center items-center w-full;
+  footer {
+    @apply mt-5;
 
-      li {
-        @apply relative list-none w-20 h-[4.375rem] z-10 cursor-pointer;
+    nav {
+      ul {
+        @apply relative flex justify-center items-center w-full;
 
-        a {
-          @apply relative flex justify-center items-center w-full;
+        li {
+          @apply relative list-none w-20 h-[4.375rem] z-10 cursor-pointer;
 
-          .icon {
-            @apply relative block text-[2em] text-light opacity-85;
-
-            transition: opacity 0.3s, transform 0.3s;
-
-            img {
-              @apply w-[1.875rem] h-[3.75rem] ml-[0.3125rem];
-
-              transition: transform 0.3s ease;
-            }
-          }
-        }
-
-        &.active {
           a {
-            .icon {
-              @apply opacity-100 -translate-y-[0.4375rem];
+            @apply relative flex justify-center items-center w-full;
 
-              filter: brightness(0) invert(12%) sepia(82%) saturate(50);
+            .icon {
+              @apply relative block text-[2em] text-light opacity-85;
+
               transition: opacity 0.3s, transform 0.3s;
+
+              img {
+                @apply w-[1.875rem] h-[3.75rem] ml-[0.3125rem];
+
+                transition: transform 0.3s ease;
+              }
+            }
+          }
+
+          &.active {
+            a {
+              .icon {
+                @apply opacity-100 -translate-y-[0.4375rem];
+
+                filter: brightness(0) invert(12%) sepia(82%) saturate(50);
+                transition: opacity 0.3s, transform 0.3s;
+              }
             }
           }
         }
-      }
 
-      .indicator {
-        @apply absolute -top-[0.625rem] w-[4.375rem] h-[4.375rem] bg-light rounded-bl-[2.1875rem] rounded-br-[2.1875rem] border-[6px] border-solid border-light cursor-pointer;
+        .indicator {
+          @apply absolute -top-[0.625rem] w-[4.375rem] h-[4.375rem] bg-light rounded-bl-[2.1875rem] rounded-br-[2.1875rem] border-[6px] border-solid border-light cursor-pointer;
 
-        transition: transform 0.5s ease;
+          transition: transform 0.5s ease;
 
-        &::before, &::after {
-          @apply absolute top-1 w-5 h-5 bg-transparent;
+          &::before, &::after {
+            @apply absolute top-1 w-5 h-5 bg-transparent;
 
-          content: "";
-        }
+            content: "";
+          }
 
-        &::before {
-          @apply -left-[1.609375rem]  rounded-tr-[1.25rem];
+          &::before {
+            @apply -left-[1.609375rem]  rounded-tr-[1.25rem];
 
-          box-shadow: 4px -6px 0 2px theme('colors.light');
-        }
+            box-shadow: 4px -6px 0 2px theme('colors.light');
+          }
 
-        &::after {
-          @apply -right-[1.609375rem]  rounded-tl-[1.25rem];
+          &::after {
+            @apply -right-[1.609375rem]  rounded-tl-[1.25rem];
 
-          box-shadow: -4px -6px 0 2px theme('colors.light');
-        }
+            box-shadow: -4px -6px 0 2px theme('colors.light');
+          }
 
-        span {
-          @apply absolute w-[3.75rem] h-[3.75rem] rounded-[50%] scale-[0.85] border-4 border-solid border-primary -left-[0.1875rem];
+          span {
+            @apply absolute w-[3.75rem] h-[3.75rem] rounded-[50%] scale-[0.85] border-4 border-solid border-primary -left-[0.1875rem];
 
-          box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+          }
         }
       }
     }
