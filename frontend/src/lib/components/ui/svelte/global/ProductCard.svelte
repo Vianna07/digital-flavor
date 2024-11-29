409f910e32c6ffcacc17bfd88bbbfc168cf7a447
Vianna07 <viannagustavo71@gmail.com>
2024-11-29 15:10:21 -0300

    fix: corrige css sem a anotação @apply do tailwind
    melhora nomes das classes de acordo com BEM
    adiciona/altera novos atributos com base nas mudanças da interface

--- frontend/src/lib/components/ui/svelte/global/ProductCard.svelte
+++ frontend/src/lib/components/ui/svelte/global/ProductCard.svelte
@@ -2,25 +2,27 @@
 	import type { ProductCardProps } from "$lib/types";
 
   let {
+      id,
       name,
-      description,
+      shortDescription,
       price,
       imageUrl,
       onclick = () => {},
   }: ProductCardProps = $props();
 </script>
 
-<div class="product-card ">
-  <img src={imageUrl} alt={name} class="product-card__image w-32 h-28 rounded-lg" />
+<div class="product-card">
+  <img src={imageUrl} alt={name} />
+
   <div class="product-card__info">
-    <h3 class="product-card__name">{name}</h3>
-    <p class="product-card__short-description">{description.slice(0, 20)}</p>
+    <h3 class="product-card__info__name">{name}</h3>
+    <p class="product-card__info__short-description">{shortDescription}</p>
   </div>
 
   <div class="product-card__purchase">
-      <h1 class="product-card__price">R$ {price.toFixed(2)}</h1>
+      <h1 class="product-card__purchase__price">R$ {price.toFixed(2)}</h1>
       <button
-        class="product-card__add-to-order"
+        class="product-card__purchase__add-to-order"
         {onclick}
       >
         +
@@ -30,26 +32,33 @@
 
 <style lang="postcss">
   .product-card {
-    @apply flex flex-col items-center rounded-lg p-3 gap-3 w-157px h-231px shadow-xl bg-light;
+    @apply flex flex-col items-center rounded-lg p-3 gap-3 w-40 h-60 shadow-xl border-2 border-secondary-300;
+
+    img {
+      @apply w-32 h-28 rounded-lg;
+    }
 
     &__info {
       @apply w-full;
+
+      &__name {
+        @apply text-base font-bold text-left text-contrast;
+      }
+      &__short-description {
+        @apply text-xs text-secondary text-left mt-0;
+      }
     }
-    &__name {
-      text-base font-bold text-left text-contrast
-    }
-    &__short-description {
-      text-xs text-secondary text-left mt-0
-    }
+
     &__purchase {
-      flex items-center justify-between w-full
-    }
-    &__price {
-      text-base font-bold text-contrast
-    }
-    &__add-to-order {
-      flex items-center justify-center bg-primary text-light border-none rounded-[30%] w-8 h-8 text-lg cursor-pointer hover:bg-primary/90 transition-colors
+      @apply flex items-center justify-between w-full;
+
+      &__price {
+        @apply text-base font-bold text-contrast;
+      }
+
+      &__add-to-order {
+        @apply flex items-center justify-center bg-primary text-light border-none rounded-[30%] w-8 h-8 text-lg cursor-pointer hover:bg-primary/90 transition-colors;
+      }
     }
   }
-
 </style>
