f94a7cde4df69e22f93f06fe96f66001b0ee8fd8
Vianna07 <viannagustavo71@gmail.com>
2024-11-28 22:13:44 -0300

    feat: cria page.server para receber dados de configuração do usuário

--- frontend/src/routes/home/settings/+page.svelte
+++ frontend/src/routes/home/settings/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import type { UserSettings } from '$lib/types';
 	import logoutIcon from '@icons/logout.svg'
 	import settingsIcon from '@icons/settings.svg'
-	let user = {
-		name: "Seu Nome",
-		email: "seu.email@mail.com"
-	};
+
+  let { data }: { data: { userSettings: UserSettings } } = $props();
+  const user = data.userSettings;
+
+  function logout() {
+    goto("/")
+  }
 </script>
 
 <div class="page">
@@ -19,14 +24,14 @@
 		</div>
 
 		<div class="actions">
-			<div class="action">
+			<button class="action">
 				<img src={settingsIcon} alt="Configuração" class="icon" />
 				<p>Editar informações</p>
-			</div>
-			<div class="action">
+			</button>
+			<button class="action" onclick={logout}>
 				<img src={logoutIcon} alt="Logout" class="icon" />
 				<p>Logout</p>
-			</div>
+			</button>
 		</div>
 	</div>
 </div>
@@ -64,8 +69,14 @@
 			.actions {
 				@apply flex flex-col gap-6;
 
-				.action {
-					@apply flex items-center gap-4;
+        .action {
+          @apply flex items-center gap-4;
+
+          transition: 200ms ease-in-out all;
+
+          &:hover {
+            @apply scale-110;
+          }
 
 					.icon {
 						filter: invert(20%) sepia(25%) saturate(100) hue-rotate(15deg);
