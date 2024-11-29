0b21ec944b1340abe92d2575a93b3205c07c9da3
Vianna07 <viannagustavo71@gmail.com>
2024-11-29 15:14:19 -0300

    feat: cria endpoint para obter cantina pelo id

--- backend/src/main/java/br/com/digital/flavor/backend/canteen/CanteenDto.java
+++ backend/src/main/java/br/com/digital/flavor/backend/canteen/CanteenDto.java
@@ -3,4 +3,7 @@ package br.com.digital.flavor.backend.canteen;
 import java.util.UUID;
 
 public record CanteenDto(UUID id, String name, String address) {
+    public CanteenDto(Canteen canteen) {
+        this(canteen.getId(), canteen.getName(),canteen.getAddress());
+    }
 }
