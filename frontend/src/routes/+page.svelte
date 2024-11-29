d720be1b6d1aeabbc886dca759f1db938738c042
Vianna07 <viannagustavo71@gmail.com>
2024-11-28 22:55:49 -0300

    feat: finaliza página inicial
    altera css puro para tailwindcss
    remove conteúdos desnecessários

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,69 +1,44 @@
 <script>
+  import logo from "@images/logo.png"
+	import { goto } from "$app/navigation";
+</script>
 
-    const handleLogin = () => alert("Entrar clicado!");
-    const handleSignUp = () => alert("Cadastrar clicado!");
-  </script>
-  
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #faf4f0;
-      margin: 0;
-      padding: 0;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-    }
-  
-    .container {
-      width: 350px;
-      padding: 20px;
-      border: 1px solid #ddd;
-      border-radius: 8px;
-      background: #fff;
-      text-align: center;
-    }
-  
-    .logo img {
-      width: 50px;
-    }
-  
-    .logo h1 {
-      font-size: 24px;
-      margin: 10px 0;
-      color: #b32a2a;
-    }
-  
-   
-  
-    button {
-      width: 100%;
-      padding: 15px;
-      margin: 10px 0;
-      border-radius: 15px;
-      font-size: 16px;
-      cursor: pointer;
-    }
-  
-    .btn-login {
-      background-color: #b32a2a;
-      color: #fff;
-    }
-  
-    .btn-signup {
-      background-color: #e0e0e0;
-      color: #b32a2a;
-    }
+<div class="center">
+  <div class="page">
+    <img src={logo} alt="Digital Flavor Logo" />
+    <h1>Bem-vindo ao Digital Flavor</h1>
 
-  </style>
-    
-  <div class="container">
-    <div class="logo">
-      <img src="/logo.png" alt="Digital Flavor Logo" />
-      <h1>Bem-vindo ao Digital Flavor</h1>
-    </div>
-    <button class="primary" on:click={handleLogin}>Entrar</button>
-    <button class="btn-signup" on:click={handleSignUp}>Cadastrar</button>
-    <div class="divider">Continue com sua conta</div>
+    <button class="primary" onclick={() => goto("/login")}>Entrar</button>
+    <button class="sign-up" onclick={() => goto("/login/register")}>Cadastrar</button>
   </div>
+</div>
+
+
+
+<style lang="postcss">
+  .page {
+    @apply gap-8 ;
+
+    img {
+      @apply w-56 h-56 mb-4;
+    }
+
+    h1 {
+      @apply text-4xl font-bold text-center text-primary;
+    }
+
+    button {
+      @apply w-full ;
+    }
+
+    .sign-up {
+      @apply text-primary rounded-xl bg-secondary-200 border-4 border-secondary-400 p-2.5 outline-none drop-shadow-lg transition-colors duration-[var(--transition-speed)];
+
+      &:hover,
+      &:focus-visible {
+        @apply bg-secondary-300;
+      }
+    }
+  }
+</style>
+
