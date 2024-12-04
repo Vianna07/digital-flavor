107c73e9bd6260711ead76df4f961de1b5d52b98
Vianna07 <viannagustavo71@gmail.com>
2024-12-04 10:53:24 -0300

    feat: cria tela de edição de produto

---
+++ frontend/src/routes/home/edit/product/[id]/+page.server.ts
@@ -0,0 +1,8 @@
+import { GET_PRODUCT_BY_ID, SAVE_PRODUCT } from '@services/product';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params, cookies, fetch }) => {
+	return GET_PRODUCT_BY_ID(params.id, cookies, fetch);
+};
+
+export const actions = SAVE_PRODUCT
---
+++ frontend/src/routes/home/edit/product/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { ProductFormProps } from '$lib/types/props/form/product-form';
+  import ProductForm from '@components/form/ProductForm.svelte';
+
+  let { data }: { data: { product: ProductFormProps } } = $props();
+
+</script>
+
+<ProductForm {...data.product} submitButtonLabel={"Editar produto"} />
