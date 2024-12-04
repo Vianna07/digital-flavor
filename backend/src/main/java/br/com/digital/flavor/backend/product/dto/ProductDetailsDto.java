72857b4415e46dee6cfeaf796aba7a8633b9f2ea
Vianna07 <viannagustavo71@gmail.com>
2024-12-04 10:55:30 -0300

    feat: adiciona atributo id na record ProductDetailsDto

--- backend/src/main/java/br/com/digital/flavor/backend/product/dto/ProductDetailsDto.java
+++ backend/src/main/java/br/com/digital/flavor/backend/product/dto/ProductDetailsDto.java
@@ -4,8 +4,8 @@ import br.com.digital.flavor.backend.product.Product;
 
 import java.math.BigDecimal;
 
-public record ProductDetailsDto(String name, String shortDescription, String description, BigDecimal price, String imageUrl) {
+public record ProductDetailsDto(String id, String name, String shortDescription, String description, BigDecimal price, String imageUrl) {
     public ProductDetailsDto(Product product) {
-        this(product.getName(), product.getShortDescription(),product.getDescription(), product.getPrice(), product.getImageUrl());
+        this(product.getId().toString(), product.getName(), product.getShortDescription(),product.getDescription(), product.getPrice(), product.getImageUrl());
     }
 }
