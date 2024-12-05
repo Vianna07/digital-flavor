77457a93965478181d5def0261983d34dd8bed0e
Vianna07 <viannagustavo71@gmail.com>
2024-12-05 06:27:31 -0300

    feat: adiciona alias para stores no svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -17,7 +17,8 @@ const config = {
 			'@images/*': './src/lib/assets/images/*',
 			'@components/*': './src/lib/components/ui/svelte/*',
 			// "@components-css/*": "./src/lib/components/ui/tailwindcss/*",
-			'@services/*': './src/routes/api/services/*'
+			'@services/*': './src/routes/api/services/*',
+      '@stores/*': './src/stores/*'
 		}
 	}
 };
