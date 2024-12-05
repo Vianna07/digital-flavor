78ad62d399a92bf332a61078a09e5fd2ed4f71e0
Luiz192512 <enz.luizgustavo@gmail.com>
2024-12-05 01:08:38 -0300

    fix: corrige erro ao passar valor padrão para textarea

--- frontend/src/lib/components/ui/svelte/global/generic/GenericTextarea.svelte
+++ frontend/src/lib/components/ui/svelte/global/generic/GenericTextarea.svelte
@@ -2,21 +2,22 @@
 	import type { GenericTextareaProps } from '$lib/types';
 
 	let { id, placeholder, required, oninput, rows = 5, value = "" }: GenericTextareaProps = $props();
+
+	const textareaAtributes = {
+		id,
+		name: id,
+		required,
+		placeholder,
+		oninput: (event: Event) => oninput?.((event.target as HTMLInputElement).value),
+		rows,
+		value: value.trim()
+	};
 </script>
 
-<textarea
-	{id}
-	name={id}
-	{required}
-	{placeholder}
-	oninput={(event: Event) => oninput?.((event.target as HTMLInputElement).value)}
-	{rows}
->
-  {value}
-</textarea>
+<textarea {...textareaAtributes}></textarea>
 
 <style lang="postcss">
 	textarea {
-		@apply relative z-10 w-full rounded-lg border-2 border-secondary-300 p-4 shadow-md outline-none focus-visible:border-primary;
+		@apply relative w-full rounded-lg border-2 border-secondary-300 p-4 shadow-md outline-none focus-visible:border-primary;
 	}
 </style>
