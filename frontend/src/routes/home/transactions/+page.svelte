d972d9bc1f418d44db3560a262f87f37ef239d58
Vianna07 <viannagustavo71@gmail.com>
2024-12-05 13:26:51 -0300

    feat: usa GenericSearchableList para iterar sobre os pedidos
    usa dados obtidos no page.server ao invés de dados fixos

--- frontend/src/routes/home/transactions/+page.svelte
+++ frontend/src/routes/home/transactions/+page.svelte
@@ -1,61 +1,73 @@
 <script lang="ts">
+	import type { GenericSearchableListProps, ListingOrder } from "$lib/types";
+	import GenericSearchableList from "@components/global/generic/GenericSearchableList.svelte";
 	import GoBack from "@components/global/GoBack.svelte";
 
-	let tabs = ["Pedidos", "Pagamentos"];
-	let activeTab = tabs[0];
+  let { data }: { data: { orders: ListingOrder[] } } = $props()
 
-	let transactions = [
-		{ name: "Giuseppe Cadura", date: "26/11/2024 - 10:05", amount: 12.50 },
-		{ name: "Mia Regarza", date: "25/11/2024 - 9:40", amount: 10.00 },
-		{ name: "Sintia Miavara", date: "25/11/2024 - 9:39", amount: 8.00 },
-		{ name: "Koy Tannal", date: "25/11/2024 - 9:39", amount: 2.50 },
-		{ name: "Kiko Lindo", date: "25/11/2024 - 9:38", amount: 5.00 }
-	];
+	// let tabs = ["Pedidos", "Pagamentos"];
+	// let activeTab = $state(tabs[0]);
 
-	function switchTab(tab: string) {
-		activeTab = tab;
-	}
+  const searchableListProps: GenericSearchableListProps<ListingOrder> = {
+    list: {
+      data: data.orders,
+      fields: {
+        title: 'userName',
+        subTitle: 'createdAt'
+      },
+      contentStyle: {
+        title: 'text-left',
+        subtitle: 'text-left'
+      },
+
+      right: {
+        snippet: right,
+        field: 'value'
+      }
+    },
+    searchInput: {
+      label: "Buscar pedido por cliente",
+    }
+  }
+
+	// function switchTab(tab: string) {
+	// 	activeTab = tab;
+	// }
 </script>
+
+{#snippet right(index: number)}
+  <p class="order-value">{(data.orders[index].value).toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+  })}</p>
+{/snippet}
 
 <div class="page">
 	<header>
-		<GoBack redirectUrl={'/home'} />
-		<h1 class="simple-title--lg">Transações</h1>
+		<h1 class="simple-title">Transações</h1>
 	</header>
 
-	<div class="tabs">
+	<!-- <div class="tabs">
 		{#each tabs as tab}
 			<button
 				class="tab {activeTab === tab ? 'active' : ''}"
-				on:click={() => switchTab(tab)}
+				onclick={() => switchTab(tab)}
 			>
 				{tab}
 			</button>
 		{/each}
-	</div>
+	</div> -->
 
-	<div class="transactions-content">
-        {#each transactions as transaction}
-            <div class="transactions-content__item">
-                <div class="transactions-content__details">
-                    <p class="transactions-content__name">{transaction.name}</p>
-                    <p class="transactions-content__date">{transaction.date}</p>
-                </div>
-                <p class="transactions-content__amount">
-                    R$ {transaction.amount.toFixed(2)}
-                </p>
-            </div>
-        {/each}
-    </div>    
+  <GenericSearchableList {...searchableListProps} />
 </div>
 
 <style lang="postcss">
-	.page {
-		@apply flex flex-col min-h-screen items-center gap-6 font-body;
+  .order-value {
+    @apply w-32 text-primary font-bold;
+  }
 
-		header {
-			@apply w-full flex justify-between items-center mt-4;
-		}
+	.page {
+		@apply flex flex-col items-center gap-6 font-body;
 
 		.tabs {
 			@apply flex justify-between gap-4 mb-6 w-full max-w-md;
@@ -67,30 +79,5 @@
 				}
 			}
 		}
-
-		.transactions-content {
-            @apply flex flex-col gap-4 w-full max-w-md;
-
-            &__item {
-                @apply flex justify-between items-center bg-light rounded-lg shadow-sm p-4;
-            }
-
-            &__details {
-                @apply flex flex-col gap-1;
-            }
-
-            &__name {
-                @apply text-sm font-medium text-contrast;
-            }
-
-            &__date {
-                @apply text-xs text-placeholder;
-            }
-
-            &__amount {
-                @apply text-sm font-bold text-primary;
-            }
-        }
-
 	}
 </style>
